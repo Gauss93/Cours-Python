fdc83b10958634c97b6e777f3efadbafad14ea8b
VotreNom <alexandredasilva1ga@gmail.com>
2026-02-16 07:11:48 +0100

    streak

--- Python/OpenC/extraction/main.py
+++ Python/OpenC/extraction/main.py
@@ -1,9 +1,36 @@
-import requests
 from bs4 import BeautifulSoup
 
-with open("index.html", 'r') as file:
+
+with open("index.html", 'r', encoding="utf-8") as file:
     soup = BeautifulSoup(file, 'html.parser')
 
 title = soup.title
+print(title)
+
 id = soup.find(id='titre')
-print(title, id)
+print(id)
+
+produit = dict()
+infos = soup.find_all("li")
+
+for info in infos:
+    name = info.find("h2").string
+    price_str = info.find("p", class_="price").string
+
+    
+    produit[name] = {"prix": price_str.strip()}
+
+    description = info.find_all("p")[-1].string
+    produit[name]["description"] = description
+
+print("Produits", produit)
+
+for name in produit.keys():
+    price_str = produit[name]["prix"]
+
+    price = price_str.split(":")[-1].replace("€", "").strip()
+    price = float(price)
+    dollar_price = price * 1.1
+    produit[name]["prix_dollar"] = f"{dollar_price}$"
+
+print("Tous les produits:", produit)
